f3aefb147241aeca70ce7a15ff0a4dfef524bd23
Philip Lanphar <philip.lanphar@plexusworldwide.com>
2018-02-02 13:05:04 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+var port = process.env.PORT || 3000;
+
+app.use(bodyParser.urlEncoded({extended: true}));
+
+app.get('/', function (req, res) {
+	res.status(200).send('Success!')
+});
+
+app.listen(port, function () {
+	console.log('Listening on port ' + port);
+});
+
+app.post('/lunch', function(req, res, next) {
+	var userName = req.body.user_name;
+	var botPayload = {
+		text: "Hello " + userName + "welcome to slack Botz!";
+	}
+
+	if (userName !== 'slackbot') {
+		return 	res.status(200).json(botPayload);
+	} else {
+		return 	res.status(200).end();
+	}
+
+
+});
+
