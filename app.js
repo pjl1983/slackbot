5829fb7137ec5f829195533479198071b28c51cf
Philip Lanphar <33945942+pjl1983@users.noreply.github.com>
2019-12-12 19:25:22 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,40 +1,35 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-
-var app = express();
-var port = process.env.PORT || 3000;
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.get('/', function (req, res) {
-	res.status(200).send('Success!')
+app.get('/', function(req, res) {
+  res.status(200).send('Success!');
 });
 
-app.listen(port, function () {
-	console.log('Listening on port ' + port);
+app.listen(port, function() {
+  console.log('Listening on port ' + port);
 });
 
 app.post('/lunch', function(req, res, next) {
-	var userName = req.body.user_name;
+  const userName = req.body.user_name;
+  const options = ['Fired Pie', 'Red Robin', 'Cold Beer & Cheezeburgers', 'Yogis', 'Gyro Express', 'Portillo\'s', 'Canton Dragon', 'Joyful Chinese', 'Famous 48', 'Naked BBQ', 'Tavern', 'Pei Wei', 'The Vig', 'The Other tavern', 'Chompies', 'Ling & Louies', 'Streets of New York', 'Grassroots', 'Zipps'];
+  let choice = 'Fired Pie';
+  (function() {
+    var number = Math.floor(Math.random() * options.length);
+    choice = options[number];
+  })();
 
-    var options = ['Fired Pie','Red Robin','Cold Beer & Cheezeburgers','Yogis','Gyro Express','Portillos','Canton Dragon','Joyful Chinese','Porkopolis','Naked BBQ','Tavern','Pei Wei','Wallys','The Vig','Humble Pie',]
-    var choice = "Fired Pie";
-    (function () {
-        var number = Math.floor(Math.random() * options.length);
-        choice = options[number];
-    })();
+  let botPayload = {
+    response_type: 'in_channel',
+    text: 'I recommend eating at ' + choice + '.',
+  };
 
-    var botPayload = {
-	response_type: "in_channel",
-        text: "I recommend eating at " + choice + "."
-    }
-
-	if (userName !== 'slackbot') {
-		return 	res.status(200).json(botPayload);
-	} else {
-		return 	res.status(200).end();
-	}
-
-
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
 });
-
